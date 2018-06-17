148d029b1a321876107c64f37f0c052f17e4877a
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-06-17 17:14:41 +0200

    Everything seems to be working fine :)
    Everything seems to be working!

--- PWA/gulpfile.js
+++ PWA/gulpfile.js
@@ -1,9 +1,37 @@
-var gulp = require('gulp');
-var packageJson = require('package.json');
-var swPrecache = require('lib/sw-precache.js');
+/**
+ * gulpfile.js for app4refs/PWA.
+ * 'watch' will not update the service worker file!
+ * Author: David Campos R. <david.campos.r96@gmail.com>
+ */
+
+var gulp        = require('gulp');
+var path        = require('path');
+var runSequence = require('run-sequence');
+var concat      = require('gulp-concat');
+//var rename      = require('gulp-rename');
+var sourcemaps  = require('gulp-sourcemaps');
+var uglifyJs    = require('gulp-uglify');
+var minifyCss   = require('gulp-minify-css');
+var htmlmin     = require('gulp-htmlmin');
+var packageJson = require('./package.json');
+var swPrecache  = require('sw-precache');
+
+var JS_DIR = 'js';
+var CSS_DIR = 'css';
 
 var DEV_DIR = 'src';
+var DEV_JS_DIR = path.join(DEV_DIR, JS_DIR);
+var DEV_CSS_DIR = path.join(DEV_DIR, CSS_DIR);
+var DEV_JS_SRC = path.join(DEV_JS_DIR, '*.js');
+var DEV_CSS_SRC = path.join(DEV_CSS_DIR, '*.css');
+var DEV_HTML_SRC = path.join(DEV_DIR, '*.html');
+
 var DIST_DIR = 'dist';
+var DIST_JS_DIR = path.join(DIST_DIR, JS_DIR);
+var DIST_JS_FILE = 'javascript.min.js';
+var DIST_CSS_DIR = path.join(DIST_DIR, CSS_DIR);
+var DIST_CSS_FILE = 'style.min.css';
+var SERVICE_WORKER_NAME = 'cache-service-worker.js';
 
 function writeServiceWorkerFile(rootDir, handleFetch, callback) {
   var config = {
@@ -41,7 +69,7 @@ function writeServiceWorkerFile(rootDir, handleFetch, callback) {
     staticFileGlobs: [
       rootDir + '/css/**.css',
       rootDir + '/**.html',
-      rootDir + '/images/**.*',
+      //rootDir + '/images/**.*',
       rootDir + '/js/**.js'
     ],
     stripPrefix: rootDir + '/',
@@ -49,10 +77,43 @@ function writeServiceWorkerFile(rootDir, handleFetch, callback) {
     verbose: true
   };
 
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
+  swPrecache.write(path.join(rootDir, SERVICE_WORKER_NAME), config, callback);
 }
 
 gulp.task('default', ['build']);
+
+gulp.task('watch', function() {
+   gulp.watch(DEV_JS_SRC, ['dist-js']);
+   gulp.watch(DEV_CSS_SRC, ['dist-css']);
+   gulp.watch(DEV_HTML_SRC, ['dist-html']);
+});
+
+gulp.task('build', function(callback) {
+  runSequence('generate-service-worker-dist', 'dist-javascript', 'dist-css', 'dist-html', callback);
+});
+
+gulp.task('dist-javascript', function(){
+  return gulp.src(DEV_JS_SRC)
+    .pipe(sourcemaps.init({largeFile: true}))
+      .pipe(concat(DIST_JS_FILE))
+      .pipe(uglifyJs({mangle: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.join(DIST_DIR, 'js')));  // saved in the DIST folder
+});
+
+gulp.task('dist-css', function(){
+  return gulp.src(DEV_CSS_SRC)
+    .pipe(concat(DIST_CSS_FILE))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(path.join(DIST_DIR, 'css')));
+});
+
+gulp.task('dist-html', function(){
+  return gulp.src(DEV_HTML_SRC)
+    //.pipe(concat('exampleallhtml.html'))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(DIST_DIR));
+});
 
 gulp.task('generate-service-worker-dist', function(callback) {
   writeServiceWorkerFile(DIST_DIR, true, callback);
@@ -60,13 +121,4 @@ gulp.task('generate-service-worker-dist', function(callback) {
 
 gulp.task('generate-service-worker-dev', function(callback) {
   writeServiceWorkerFile(DEV_DIR, false, callback);
-});
-
-gulp.task('build', function(callback) {
-  runSequence('copy-dev-to-dist', 'generate-service-worker-dist', callback);
-});
-
-gulp.task('copy-dev-to-dist', function() {
-  return gulp.src(DEV_DIR + '/**')
-    .pipe(gulp.dest(DIST_DIR));
 });
