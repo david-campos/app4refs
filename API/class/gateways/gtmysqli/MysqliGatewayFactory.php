ce00f06066769f5f28dbf8c2be3ff03fa129c079
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-07-04 19:23:02 +0200

    Irrelevant stuff I want to save
    Nothing to see here :P

--- API/class/gateways/gtmysqli/MysqliGatewayFactory.php
+++ API/class/gateways/gtmysqli/MysqliGatewayFactory.php
@@ -20,7 +20,7 @@ class MysqliGatewayFactory extends GatewayFactory {
      * MysqliGatewayFactory constructor.
      */
     public function __construct() {
-        $this->mysqli = new mysqli('localhost', 'root', '', 'pruebas_tfg');
+        $this->mysqli = new mysqli('localhost', 'root', '', 'para_eliminar');
         if ($this->mysqli->connect_error) {
             throw new DatabaseInternalException('Database connection error (' . $this->mysqli->connect_errno . ') '
                 . $this->mysqli->connect_error);
